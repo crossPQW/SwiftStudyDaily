1a9ccaa5cf520579b5f5a0cb5d0a58058791d780
crossPQW <651024752@qq.com>
2015-12-24 01:10:09 +0800

    第一章基本部分学习完毕
    文章地址:http://wiki.jikexueyuan.com/project/swift/chapter2/01_The_Basics.ht
    ml

--- SwiftStudyDaily/ViewController.swift
+++ SwiftStudyDaily/ViewController.swift
@@ -13,8 +13,109 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.base()
     }
 
+    //base 基础部分
+    func base () ->Void{
+        //变量在使用之前需要进行生命,通常我们用let 声明常量,用var声明变量
+        let number = 100 //不可修改
+        _  = 200//可以修改
+        
+        var x = 100,y = 200, z = 300
+        
+        var string: String = "hello world"
+        
+        var a, b, c: String
+        
+        var aString: String = "hello"
+        aString .appendContentsOf("world")
+        print(aString)
+        
+        print("the current value is \n(hello world) \n and the value is \(aString)")
+        
+        let min8 = UInt8.min
+        let max8 = UInt8.max
+        print("min is \(min8),max is \(max8)")//Uint8
+        
+        typealias hhh = String
+        var heiheihei: hhh = "asdasdadas"
+        print(heiheihei)
+        
+        var isOpen = true
+        if isOpen{
+            print("true")
+        }
+        else
+        {
+            print("false")
+        }
+        
+//        let error = (404, "NOT FOUND")
+//        print(error.0,error.1)
+//        
+//        let (code,message) = error
+//        print(code,message)
+        
+        let httpError = (code: 200, message:"OK")
+        print(httpError.code,httpError.1)
+        
+        var http404Code :Int? = 404
+        http404Code = nil
+        
+        
+        let possibleNumber = "111111"
+        let convertNumber = Int(possibleNumber)
+        
+        if convertNumber == nil{
+            print("isnil")
+        }else{
+            print("nonnull")
+        }
+        
+        if convertNumber != nil{
+            print("it is has value is \(convertNumber!)")//如果一个可选值确定有值可以用!强制解析
+        }
+        
+        //可选绑定
+//        if let constant = someOption{
+//            statement
+//        }
+        if let actualNumber = convertNumber{
+            print("\(actualNumber) is \(convertNumber)")
+        }else{
+//            print("\(actualNumber) is not  \(convertNumber)") //这里会报错,因为转换成功后只能在第一个分支里面使用转换好的变量
+        }
+        
+        if let firstNumber = Int("100"), secondNumber = Int("100000") where firstNumber < secondNumber
+        {
+            print("\(firstNumber) < \(secondNumber)")
+        }
+        
+        func canthrowerror() throws{
+            //这个函数可能抛出错误
+        }
+        do{
+            try canthrowerror()
+            
+        }catch{
+            
+        }
+        
+        func makeerror() throws{
+            
+        }
+        do{
+            try makeerror()
+        }catch {
+            
+        }
+        
+        let age = 3
+        assert(age < 3, "age is less than 3")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
